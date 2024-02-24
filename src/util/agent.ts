105eee207eed760264f9c3f9ec46296ce7c47b02
Andromeda Yelton <andromeda.yelton@gmail.com>
2024-02-24 11:00:30 -0500

    alas that my home editor doesn't have ts language support

--- src/util/agent.ts
+++ src/util/agent.ts
@@ -1,4 +1,5 @@
-export default async getAgent() => {
-  const agent = new AtpAgent({ service: `${process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT}` })
-  await agent.login({ identifier: handle, password })
+export async function getAgent() {
+  const agent = new AtpAgent({ service: `${process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT}` });
+  await agent.login({ identifier: handle, password });
+  return agent
 }
